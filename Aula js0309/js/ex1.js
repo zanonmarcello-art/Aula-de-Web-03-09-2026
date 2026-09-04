be4048400cf48a8cde93bc9d0e0186a58bff30e0
zanonmarcello-art <zanon.marcello@gmail.com>
2026-09-03 22:22:35 -0300

    Add files via upload

---
+++ Aula js0309/js/ex1.js
@@ -0,0 +1,7 @@
+//varoaveis
+//
+var nome = prompt("Digite seu nome");
+
+//Comando escrever
+console.log("Seu nome é:" , nome);
+alert("Seu nome é: "+ nome);
---
+++ Aula js0309/js/ex2.js
@@ -0,0 +1,7 @@
+//  Declarar variável
+var x = parseFloat(prompt("Digite o valor de x: "));
+var y = parseFloat(prompt("Digite o valor de y: "));;
+//processamento
+var soma = x + y;
+// saída de dados
+console.log("Valor da soma é: ", soma);
